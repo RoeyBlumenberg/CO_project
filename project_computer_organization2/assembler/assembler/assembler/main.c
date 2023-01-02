2a56cfda4d00cadc166e57ec515a3b359e76e6af
RoeyBlumenberg <118200269+RoeyBlumenberg@users.noreply.github.com>
2023-01-02 18:49:02 +0200

    Add files via upload

---
+++ project_computer_organization2/assembler/assembler/assembler/main.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include "assembler.h"
+
+int main(int argc, char *argv[]) {
+
+	FILE *asm_file, *memin ;
+	Label Labels[NUM_MEMIN_LINES];//changed
+	Intruction Instructions[NUM_MEMIN_LINES];
+	Word_instruction Words[NUM_MEMIN_LINES];
+	init_Labels(Labels);
+	init_Instructions(Instructions);
+	init_Words(Words);
+	asm_file =fopen( "fib.asm", "r");
+	first_pass(asm_file, Labels);
+	second_pass(asm_file, Instructions, Words);
+	close_file(asm_file);
+	// write to memin.txt
+	memin = open_file("memim.txt", "w");
+	print_memin(memin, Instructions, Labels);
+	close_file(memin);
+	// write to dmemin.txt
+	//dmemin = open_file(argv[3], "w");
+	//print_dmem(dmemin, Words);
+	//close_file(dmemin);
+	return 0;
+}
